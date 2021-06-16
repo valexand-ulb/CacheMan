047bd71785b85400a4bddb819985c594b3a62d37
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2021-06-16 02:09:21 +0200

    ajout d'une liste de tuple ( vecteur -> haut ,  bas ,gauche ,droite)

--- position.hpp
+++ position.hpp
@@ -5,14 +5,18 @@
 #ifndef POSTION_POSITION_HPP
 #define POSTION_POSITION_HPP
 
+#include <tuple>
 
 class Position {
 private:
-    int x,y;
+    int x,y;    // position
+    std::tuple<int,int> vectors[4] = {{0,-1},{0,1},{-1,0},{1,0}};
+
 public:
     Position(int x, int y);
     int getX() const, getY() const;
     int distFromPos(Position pos) const;
+
 };
 
 
