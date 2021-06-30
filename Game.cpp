adf9081941b11a2b594991c6d13879a4f769cefc
Arcool <56786979+Arcools-ulb@users.noreply.github.com>
2021-06-30 11:12:47 +0200

    Add files via upload

--- Game.cpp
+++ Game.cpp
@@ -3,9 +3,36 @@
 
 void Game::initGame(){
 
-    //player1 = new Player(50,0);
-    //ghost1 = new Ghost(50,1);
+    
     map = new Matrice("Map/level1.txt");
+    std::string posi = map->get_spawn_person();
+
+    std::stringstream strValue;
+    strValue << posi[0];
+
+    unsigned int x;
+    strValue >> x;
+
+    strValue << posi[2];
+
+    unsigned int y;
+    strValue >> y;
+
+    player1 = new Player(50, x, y,0);
+
+    std::string posi = map->get_spawn_person();
+    strValue << posi[0];
+
+    unsigned int x;
+    strValue >> x;
+
+    strValue << posi[2];
+
+    unsigned int y;
+    strValue >> y;
+    ghost1 = new Ghost(50,x,y,1);
+    mainBoucle();
+    return;
 }
 
 
