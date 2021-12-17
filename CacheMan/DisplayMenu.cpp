e4ae7adfbb85ea4109e97a38555ca81de76b5064
Arcool <56786979+Arcools-ulb@users.noreply.github.com>
2021-12-17 18:58:04 +0100

    Delete CacheMan directory
    Petit fail

--- CacheMan/DisplayMenu.cpp
+++
@@ -1,9 +0,0 @@
-#include "DisplayMenu.hpp"
-
-void DisplayMenu::display(){
-    std::string input;
-    std::cout << "Bienvenue dans CacheMan"<< std::endl;
-    std::cout << "ecrivez play pour jouer"<< std::endl;
-    std::cin >> input;
-    //lance le jeu
-}
