7eb4c34c1dbd69659615e5f339a4c73874a18765
Alan Vanderhoeven <alan.vanderhoeven@gmail.com>
2022-01-30 22:32:34 +0100

    Les peronnages ne sortent plus de la matrice 20x20 dans le terminal (bug avec SFML - mauvais mouvement + spawn en dehors du plateau)

--- Person.cpp
+++ Person.cpp
@@ -3,32 +3,33 @@
 
 void Person::move(int direction){
     std::cout << "Le bon mouvement se fait" << std::endl;
-    if (direction == UP and isMove(_pos->getX(), _pos->getY()+_speed)){
+    //std::cout << "##############################################" << std::endl;
+    //std:: cout <<  _pos->getX() << " and " << _pos ->getY() << std::endl;
+    if (direction == UP and isMove(_pos->getX()-_speed, _pos->getY())){
         _pos->setX(_pos->getX()-_speed);
     }
-    else if(direction == DOWN and isMove(_pos->getX(), _pos->getY()-_speed)){
+    else if(direction == DOWN and isMove(_pos->getX()+_speed, _pos->getY())){
         _pos->setX(_pos->getX()+_speed);
     }
-    else if(direction == LEFT and isMove(_pos->getX()-_speed, _pos->getY())){
-        //_pos->setY(_pos->getY()+_speed);
+    else if(direction == LEFT and isMove(_pos->getX(), _pos->getY()-_speed)){
         _pos->setY(_pos->getY()-_speed);
     }
-    else if(direction == RIGHT and isMove(_pos->getX()+_speed, _pos->getY())){
-        //_pos->setY(_pos->getY()-_speed);
+    else if(direction == RIGHT and isMove(_pos->getX(), _pos->getY()+_speed)){
         _pos->setY(_pos->getY()+_speed);
     }
-    std:: cout <<  _pos->getX() << "and " << _pos ->getY() << std::endl;
+    
+
 }
 
 bool Person::isMove(int x, int y){
-    //std::cout << "next move will be: x = " << x << " and y = " << y << std::endl;
+    std::cout << "next move will be: x = " << x << " and y = " << y << std::endl;
     // ne fonctionne pas correctement
-    /*if ((x > 0 and x < 20) and (y > 0 and y < 20)) {
+    if ((x >= 1 and x <= 18) and (y >= 1 and y <= 18)) {
         return true;
     }
     else {
         return false;
     }
-    */
+    
    return true;
 }
